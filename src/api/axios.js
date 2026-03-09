87fae6e4c32451ce7d9af00bb51d404ff02a6f3f
vishalsinha2004 <vishalsinha6567@gmail.com>
2026-03-10 01:53:00 +0530

    fix url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'http://127.0.0.1:8000/api/',
-    baseURL: 'https://parceel.onrender.com',
+    baseURL: 'http://127.0.0.1:8000/api/',
 });
 
 api.interceptors.request.use((config) => {
